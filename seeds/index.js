3fb1bc28406e3bf4b0795131d5c4e13ffb14a73f
Rooster <gallolopez1@hotmail.com>
2022-01-13 18:09:46 -0500

    Updated seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,26 +1,26 @@
-const seedComments = require('./comment-seeds');
-const seedRatings = require('./ratings-seeds');
 const seedUsers = require('./user-seeds');
 const seedPosts = require('./post-seeds');
+const seedComments = require('./comment-seeds');
+const seedVotes = require('./vote-seeds');
 
 const sequelize = require('../config/connection');
 
-const seedAll = async() => {
-    await sequelize.sync({ force: true });
-    console.log('--------------');
-    await seedUsers();
-    console.log('--------------');
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('--------------');
+  await seedUsers();
+  console.log('--------------');
 
-    await seedPosts();
-    console.log('--------------');
+  await seedPosts();
+  console.log('--------------');
 
-    await seedComments();
-    console.log('--------------');
+  await seedComments();
+  console.log('--------------');
 
-    await seedRatings();
-    console.log('--------------');
+  await seedVotes();
+  console.log('--------------');
 
-    process.exit(0);
+  process.exit(0);
 };
 
 seedAll();
