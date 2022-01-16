360376f04fe91d3bdfbfcd724b6afe6099c63e69
Rooster <gallolopez1@hotmail.com>
2022-01-15 21:15:05 -0500

    Updated seeds structure, still need to modify content

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,6 @@
 const seedUsers = require('./user-seeds');
 const seedPosts = require('./post-seeds');
 const seedComments = require('./comment-seeds');
-const seedRatings = require('./ratings-seeds');
 
 const sequelize = require('../config/connection');
 
@@ -15,9 +14,6 @@ const seedAll = async() => {
     console.log('--------------');
 
     await seedComments();
-    console.log('--------------');
-
-    await seedRatings();
     console.log('--------------');
 
     process.exit(0);
