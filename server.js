bd84469b37c148c3629190b570f563976acb82f6
Brian <BMArsenault@gmail.com>
2022-01-15 23:32:42 -0700

    Removed filesupload from server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const fileUpload = require('express-fileupload');
+// const fileUpload = require('express-fileupload');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -32,10 +32,10 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/form', express.static(__dirname + 'public'));
+// app.use('/form', express.static(__dirname + 'public'));
 
 app.use(require('./controllers/'));
-app.use(fileUpload());
+// app.use(fileUpload());
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
